43c3490e228d23e9d41cbad916d274b65d292f50
CaioDetz <caiodetz654@gmail.com>
2023-05-25 20:51:54 +0000

    UP

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+from playwright.sync_api import sync_playwright
 import pandas as pd
 import json
+import time
 
 
 def genMSG(aluno):
@@ -12,12 +14,31 @@ def genMSG(aluno):
 historicos = pd.read_csv("./HISTORICOS.csv")
 historicos = json.loads(historicos.to_json(orient='table'))
 db = historicos['data']
-file = open("result.txt", "a")
-for item in db:
-    print(item['Nome'])
-    msg = genMSG(item)
-    file.write(msg)
-    file.write("\n\n")
 
 
-file.close()
+with sync_playwright() as p:
+    browser = p.chromium.launch()
+    page = browser.new_page()
+    page.goto("https://webmail.sorriso.mt.gov.br/")
+    page.locator(
+        '//*[@id="rcmloginuser"]').fill("emsaodomingos@sorriso.mt.gov.br")
+    page.locator('//*[@id="rcmloginpwd"]').fill('')
+    page.locator('//*[@id="submitloginform"]').click()
+    time.sleep(5)
+    print(page.title())
+    page.goto('https://webmail.sorriso.mt.gov.br/?_task=mail&_action=compose')
+    time.sleep(5)
+    print(page.title())
+    page.locator('//*[@id="_to"]').fill('caiodetz654@gmail.com')
+    time.sleep(1)
+    page.locator('#compose-subject').fill(
+        "Solicitação de Envio de Historico Escolar Escola Municipal São Domingos - Sorriso- MT")
+    time.sleep(1)
+    page.frame_locator('#composebody_ifr').locator(
+        '//*[@id="tinymce"]/p[1]').click()
+    page.keyboard.type("TESTE\nTESTE")
+    time.sleep(1)
+    page.locator('#rcmbtn108').click()
+    time.sleep(10)
+    print(page.title())
+    browser.close()
