61de6f63f06c08a99ca230c431d96d237e77edb9
CaioDetz <caiodetz654@gmail.com>
2023-05-29 14:53:04 +0000

    UP

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-from playwright.sync_api import sync_playwright
 import pandas as pd
 import json
-import time
+
 
 
 def genMSG(aluno):
 
-    msg = f"""Olá Boa Tarde Sou o Secretario da Escola Municipal São Domingos de Sorriso - MT Preciso do Histórico Escolar de Alguns Ex-alunos da sua escola ({aluno['escola_origem']} - {aluno['cidade_origem']}):\nNome dos Alunos - Data de Transferencia:\n{aluno['Nome']} Trasferido em: {aluno['data_transf']}\nAtt."""
+    msg = f"""Email:{aluno["email"]}\nAssunto: Requerimento de Historico Escolar E.M. São Domingos de Sorriso - MT\n\nOlá Boa Tarde Sou o Secretario da Escola Municipal São Domingos de Sorriso - MT Preciso do Histórico Escolar de Alguns Ex-alunos da sua escola ({aluno['escola_origem']} do municipio de {aluno['cidade_origem']}):\nNome dos Alunos - Data de Transferencia:\n{aluno['alunos']}\nAtt."""
 
     return msg
 
@@ -15,30 +14,86 @@ historicos = pd.read_csv("./HISTORICOS.csv")
 historicos = json.loads(historicos.to_json(orient='table'))
 db = historicos['data']
 
+escolas_com_email = [
+"U.E. COMUNITARIA CODÓ NOVO",
+"E.M. BOAS NOVAS",
+"E.M. PEDRO ABREU",
+"U.I. JOSE ARAUJO BEZERRA",
+"U.E.B. JOSEFINA SERRÃO",
+"C.A. NIZETH OLIVEIRA RIBEIRO",
+"E.M.E.F. TEREZINHA DE JESUS",
+"E.M. PRINCESA ISABEL",
+"U.E.M. RIO PRETO",
+"U.E.M. MARCELO THADEU",
+"E.M.E.F. ERICO VERISSIMO",
+"E.M.E.F. VINICIUS DE MORAES",
+"E.M. TANCREDO NEVES",
+"UNIDADE ESCOLAR SANTA LUIZA",
+"E.M. 13 DE MAIO",
+"U.I. ALZIRA CESAR MENESES",
+"E.M. GUSTAVO ADOLFO WILKE",
+"E.M.E.B. RODRIGO DAMASCENO",
+"E.M.E.F. JOAO PRUDENCIO DE BRITO",
+"E.M. PEDRO LIMA",
+"E.R.M. VITOR VALENDOLR",
+"E.M.E.F. EJA ANGELO DEBIASE",
+"E.M. SANTOS DUMONT",
+"E.M. MANOEL JOSE DA SILVA"
 
-with sync_playwright() as p:
-    browser = p.chromium.launch()
-    page = browser.new_page()
-    page.goto("https://webmail.sorriso.mt.gov.br/")
-    page.locator(
-        '//*[@id="rcmloginuser"]').fill("emsaodomingos@sorriso.mt.gov.br")
-    page.locator('//*[@id="rcmloginpwd"]').fill('')
-    page.locator('//*[@id="submitloginform"]').click()
-    time.sleep(5)
-    print(page.title())
-    page.goto('https://webmail.sorriso.mt.gov.br/?_task=mail&_action=compose')
-    time.sleep(5)
-    print(page.title())
-    page.locator('//*[@id="_to"]').fill('caiodetz654@gmail.com')
-    time.sleep(1)
-    page.locator('#compose-subject').fill(
-        "Solicitação de Envio de Historico Escolar Escola Municipal São Domingos - Sorriso- MT")
-    time.sleep(1)
-    page.frame_locator('#composebody_ifr').locator(
-        '//*[@id="tinymce"]/p[1]').click()
-    page.keyboard.type("TESTE\nTESTE")
-    time.sleep(1)
-    page.locator('#rcmbtn108').click()
-    time.sleep(10)
-    print(page.title())
-    browser.close()
+]
+
+"""
+{
+    'index': 287,
+    'Nº Ch.': 30,
+    'Nome': 'ISAAC MATIAS RIBEIRO',
+    'Situação': None, 
+    'Fone 1': 66999241185.0,
+    'Fone 2': None,
+    'Turma': '7º ANO H',
+    'deve_documento': 'HISTORICO',
+    'escola_origem': 'E.M. VALTER LEITE PEREIRA',
+    'cidade_origem': None,
+    'email': None,
+    'telefone': None,
+    'data_transf': '30/11/2020',
+    'solicitado': None,
+    'observação': None
+}
+"""
+dados = []
+for e in escolas_com_email:
+    alunos = []
+    cidade_origem = ""
+    email = ""
+    for i in db:
+        if i['escola_origem'] == e:
+            alunos.append("{} Trasferido em: {}".format(i['Nome'],i['data_transf']))
+            cidade_origem = i['cidade_origem']
+            email = i['email']
+    
+    
+    alunos = tuple(alunos)
+    alunoMSG = "\n".join(alunos)
+
+
+
+    dados.append({
+        'escola_origem':e,
+        'cidade_origem':cidade_origem,
+        'alunos':alunoMSG,
+        'email':email
+    })
+
+
+
+
+
+doc = open('results.txt', 'a')
+
+for e in dados:
+    msg = genMSG(e)
+    doc.write(msg)
+    doc.write("\n--------------------------------------------\n")
+
+doc.close()
---
+++ turple.py
@@ -0,0 +1,4 @@
+a_list = ["RAYSSA VITORIA JARA CARDOSO DA SILVA Trasferido em: 20/01/2023","JHEFERSON WILLYAN SOUSA DE CARVALHO Trasferido em: 15/12/2022"]
+b_tuple = tuple(a_list)
+print(b_tuple)
+print("\n".join(b_tuple))
